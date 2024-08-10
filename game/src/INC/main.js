158b86bb786b2583dc82522c7197c560d0ab4d22
Jeremy Latham <38564782+chargenp@users.noreply.github.com>
2024-08-10 13:36:02 -0400

    Dwarves intro / File status bar
    Added the basis for dwarves and moved the file management to a navbar at the bottom, along with basic CSS for the navbar

--- game/src/INC/main.js
+++ game/src/INC/main.js
@@ -1,7 +1,10 @@
 var gameData = { 
     gold: 0,
     goldPerClick: 1,
-    goldPerClickCost: 10
+    goldPerClickCost: 10,
+    dwarves: 0,
+    goldCostPerDwarf: 100,
+    goldMinedPerDwarf: 10
 }
 
 function mineGold() {
@@ -20,26 +23,50 @@ function buyGoldPerClick() {
     }
 }
 
+function buyDwarves() {
+    if (gameData.gold >= gameData.goldCostPerDwarf) {
+        gameData.gold -= gameData.goldCostPerDwarf
+        gameData.dwarves += 1
+        gameData.goldCostPerDwarf *= 2
+        document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+        document.getElementById("dwarves").innerHTML = "Buy Dwarves (Currenty have " 
+            + gameData.dwarves + ") Cost: " + gameData.goldCostPerDwarf + " Gold"
+    }
+}
+
+function updateStatus (txt) {
+    document.getElementById("statusText").setAttribute("class", "text-fade");
+    setTimeout(() => {
+        document.getElementById("statusText").innerHTML = txt;
+        document.getElementById("statusText").setAttribute("class", "text-show");
+    }, 500);
+    setTimeout(() => {
+        document.getElementById("statusText").setAttribute("class", "text-fade");
+        setTimeout(() => {
+            document.getElementById("statusText").innerHTML = "";
+        }, 500);      
+    }, 2000);
+}
+
 function saveGame() {
     var save = gameData;
     localStorage.setItem("save", JSON.stringify(save));
+    updateStatus("Game Saved");
 }
 
 function loadGame() {
     var savegame = JSON.parse(localStorage.getItem("save"));
     if (savegame != null) {
         gameData = savegame;
-        console.log("gameData updated")
-        console.log("savegame")
-        console.log(savegame)
-        console.log("gameData")
-        console.log(gameData)
     }
     updateUI();
+    updateStatus("Game Loaded");
 }
 
 function deleteSave() { 
     localStorage.removeItem("save");
+    window.location.reload();
+    updateStatus("Game Reset");
 }
 
 function updateUI() {
@@ -47,6 +74,8 @@ function updateUI() {
     document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
     document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currenty level " 
         + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+    document.getElementById("dwarves").innerHTML = "Buy Dwarves (Currenty have " 
+        + gameData.dwarves + ") Cost: " + gameData.goldCostPerDwarf + " Gold"
 }
 
 var saveGameLoop = window.setInterval(function() {
@@ -54,7 +83,12 @@ var saveGameLoop = window.setInterval(function() {
 },15000)
 
 var mainGameLoop = window.setInterval(function() {
-    mineGold()
+    dwarfMiners()
 }, 1000)
+
+function dwarfMiners() {
+    gameData.gold += gameData.dwarves * gameData.goldMinedPerDwarf
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined";
+}
 
 window.onload = loadGame();
